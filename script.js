60d279b2e09bfc0b7c26c50240433deef1e0556a
Rambabu <140634639+rambabu244@users.noreply.github.com>
2024-03-18 16:37:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".text", {
+    strings: ["Programming" , "Java Developer" , "Web Development" , "Data Analyst"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
